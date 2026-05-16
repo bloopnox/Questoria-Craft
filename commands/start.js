956f461e5e3125b36a3429b3de7a50771f395b9e
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-16 14:53:43 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -1,7 +1,21 @@
-module.exports = {
-  name: "start",
-  description: "Start the game",
-  execute: async (ctx) => {
-    await ctx.reply("🔥 Welcome to Demon Slayer Bot!\nUse /help to see commands.");
-  }
+module.exports = (bot) => {
+  bot.onText(/\/start/, async (msg) => {
+    const chatId = msg.chat.id;
+
+    await bot.sendPhoto(
+      chatId,
+      "https://wallpapercave.com/wp/wp12009969.jpg",
+      {
+        caption: `⚔️ *WELCOME TO DEMON SLAYER BOT* ⚔️
+
+🔥 Collect powerful slayers
+👹 Defeat dangerous demons
+🎒 Build your inventory
+🏆 Become the strongest warrior
+
+Use /help to begin your journey.`,
+        parse_mode: "Markdown"
+      }
+    );
+  });
 };
