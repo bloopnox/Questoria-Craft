2fe02529e2722e2cdaae0d5373689c79a1020f0d
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-17 15:32:24 +0530

    Create weapons.js

---
+++ commands/weapons.js
@@ -0,0 +1,24 @@
+const weapons = [
+  {
+    name: "🗡 Rusty Blade",
+    damage: 10,
+    rarity: "Common",
+  },
+  {
+    name: "⚔️ Nichirin Sword",
+    damage: 25,
+    rarity: "Rare",
+  },
+  {
+    name: "🔥 Flame Katana",
+    damage: 40,
+    rarity: "Epic",
+  },
+  {
+    name: "🌙 Moon Blade",
+    damage: 60,
+    rarity: "Legendary",
+  },
+];
+
+module.exports = weapons;
