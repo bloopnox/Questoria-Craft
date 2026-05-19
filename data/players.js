9d9a32fc2817dbff72db662bd7626cda909cf197
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-19 15:55:08 +0530

    Update players.js

--- data/players.js
+++ data/players.js
@@ -1,3 +1,12 @@
-const players = {};
+const fs = require("fs");
+
+let players = require("./players.json");
+
+players.save = () => {
+  fs.writeFileSync(
+    "./data/players.json",
+    JSON.stringify(players, null, 2)
+  );
+};
 
 module.exports = players;
