f4c6ff6e9752ad0c01e69e23a270fe62cf0b9c75
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-19 15:55:25 +0530

    Update guild.js

--- data/guild.js
+++ data/guild.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const fs = require("fs");
+
+let guilds = require("./guilds.json");
+
+guilds.save = () => {
+  fs.writeFileSync(
+    "./data/guilds.json",
+    JSON.stringify(guilds, null, 2)
+  );
+};
+
+module.exports = guilds;
