6d289dfe73cfcf8368e02b38f54f0608f650e02f
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-19 13:07:13 +0530

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -17,7 +17,7 @@ module.exports = (bot) => {
     try {
       await bot.sendPhoto(
         chatId,
-        "https://kommodo.ai/i/ip5xqtnqGqXGpuQxOpkZ",
+        "https://i.pinimg.com/736x/e1/97/3e/e1973e8421e69bc09f731b60f5102d97.jpg",
         {
           caption: "⚔️ WELCOME TO DEMON SLAYER BOT ⚔️"
         }
