3392628a62f43ebb411177c6a443440ac0f2bd36
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-25 11:03:26 +0530

    Create godchar.js

---
+++ asset/godchar.js
@@ -0,0 +1,41 @@
+module.exports = {
+    yoriichi_godtier: {
+        name: "Yoriichi Tsugikuni",
+        rarity: "God-Tier",
+        power: 5000,
+        cost: 499,
+        type: "Sun Breathing",
+        image: "https://i.pinimg.com/736x/5e/fc/ba/5efcba72d1d495db3bd0340913ea80a0.jpg",
+        level: 1,
+        xp: 0,
+        max_xp: 1000,
+        isAwakened: false,
+        awakeningStage: 0
+    },
+    muzan_godtier: {
+        name: "Muzan Kibutsuji",
+        rarity: "God-Tier",
+        power: 4500,
+        cost: 399,
+        type: "Demon Blood",
+        image: "https://i.pinimg.com/736x/4a/3b/a8/4a3ba8b84645185055a4469e85c62173.jpg",
+        level: 1,
+        xp: 0,
+        max_xp: 1000,
+        isAwakened: false,
+        awakeningStage: 0
+    },
+    kokushibo_godtier: {
+        name: "Kokushibo",
+        rarity: "God-Tier",
+        power: 4000,
+        cost: 199,
+        type: "Moon Breathing",
+        image: "https://i.pinimg.com/1200x/a7/e1/d2/a7e1d2f6a2528d2d0e27a4c347ec2f55.jpg",
+        level: 1,
+        xp: 0,
+        max_xp: 1000,
+        isAwakened: false,
+        awakeningStage: 0
+    }
+};
