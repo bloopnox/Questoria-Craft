aa9f993fd32518eb5da908afe1e7be913ee438ff
Vaibhav <116061881+Vaibhav19951@users.noreply.github.com>
2026-05-26 15:15:11 +0530

    Update aiEngine.js

--- core/aiEngine.js
+++ core/aiEngine.js
@@ -7,7 +7,7 @@ const genAI = new GoogleGenerativeAI(
 );
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-1.5-flash-8b",
 });
 
 async function askJarvis(prompt) {
